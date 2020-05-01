00970de2e18bfe239b0c13c40dc08d3d5d4f8698
msameh-93 <msame.93@hotmail.com>
2020-05-01 22:09:23 +0200

    Initial commit

---
+++ PhotoAppApiAccMngmt/src/test/java/com/sameh/photoapp/api/accounts/PhotoAppApiAccMngmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sameh.photoapp.api.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiAccMngmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ PhotoAppApiUsers/src/main/java/com/sameh/photoapp/api/users/controller/UsersController.java
@@ -0,0 +1,16 @@
+package com.sameh.photoapp.api.users.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UsersController {
+	
+	
+	@GetMapping("/status/check")
+	public String status() {
+		return "Working...";
+	}
+}

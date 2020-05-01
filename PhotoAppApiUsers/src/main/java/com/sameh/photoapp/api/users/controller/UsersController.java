53b44f6e1255c862a7c2f148cec7c3461a67e643
msameh-93 <msame.93@hotmail.com>
2020-05-02 00:00:12 +0200

    zuul gateway api

--- PhotoAppApiUsers/src/main/java/com/sameh/photoapp/api/users/controller/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/sameh/photoapp/api/users/controller/UsersController.java
@@ -1,5 +1,7 @@
 package com.sameh.photoapp.api.users.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/users")
 public class UsersController {
-	
+	@Autowired
+	private Environment env;
 	
 	@GetMapping("/status/check")
 	public String status() {
-		return "Working...";
+		return "Working on port: " + env.getProperty("local.server.port");
 	}
 }
